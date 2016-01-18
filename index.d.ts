bf98caad7e955bbbfd63d47e087d2570ee28ecec
vakrilov <alexander.vakrilov@gmail.com>
2016-01-18 17:02:29 +0200

    typings fix

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,8 @@
-declare module "native-script-accelerometer" {
-    export interface AccelomenterData {
-        x: number;
-        y: number;
-        z: number;
-    }
-
-    export function startAccelometerUpdates(callback: (AccelomenterData) => void);
-    export function stopAccelometerUpdates();
+export interface AccelomenterData {
+    x: number;
+    y: number;
+    z: number;
 }
+
+export function startAccelometerUpdates(callback: (AccelomenterData) => void);
+export function stopAccelometerUpdates();
